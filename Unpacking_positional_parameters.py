d942ca06493b6f7ba98f401997afeb05998243ba
Vkovriakov <walja.kovrjakov@yandex.ru>
2024-12-09 18:30:33 +0300

    Update Unpacking_positional_parameters.py

--- Unpacking_positional_parameters.py
+++ Unpacking_positional_parameters.py
@@ -7,6 +7,6 @@ print_params(c=[1, 2, 3])
 values_list = [1.0, 'строка', True]
 values_dict = {'a': 1, 'b': 'строка', 'c': True}
 print_params(*values_list)
-print_params(values_dict)
+print_params(**values_dict)
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
